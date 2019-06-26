698109b4b80e38ef40092f431d5e846e1689feda
cffycls <cffycls@foxmail.com>
2019-06-26 18:25:11 +0800

    Delete auto.sh
    no use

--- php/auto.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-PHP_VERSION='7.3.6'
-PHP_URL='https://secure.php.net/get/php-$PHP_VERSION.tar.xz/from/this/mirror'
-PHPIZE_DEPS='autoconf dpkg-dev dpkg file g++ gcc libc-dev make pkgconf re2c'
-PHPIZE_DEVS='argon2-dev coreutils curl-dev libedit-dev libsodium-dev libxml2-dev openssl-dev sqlite-dev libjpeg-turbo-dev libpng-dev gd-dev gettext-dev freetype-dev libxpm-dev libevent-dev'
