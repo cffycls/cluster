1a446e4b61d01d7f7923173a9312538d412e8383
cffycls <cffycls@foxmail.com>
2019-12-28 09:37:09 +0800

    redis端口修正，php fpm.log文件提交-防止文件夹化

--- rec.redis.sh
+++ rec.redis.sh
@@ -2,15 +2,16 @@
 docker stop rm rs && docker rm rm rs
 
 docker run --name rm \
-       	--restart=always \
+	-p 6379:6379 \
 	--network=mybridge --ip=172.1.13.11 \
 	-v /home/wwwroot/cluster/redis/data:/data \
 	-v /home/wwwroot/cluster/redis/redis.conf:/etc/redis/redis.conf \
 	-v /home/wwwroot/cluster/redis/sentinel.conf:/etc/redis/sentinel.conf \
 	-d cffycls/redis5:1.7
 
+# --restart=always 
+
 docker run --name rs \
-	--restart=always \
 	--network=mybridge --ip=172.1.13.12 \
 	-v /home/wwwroot/cluster/redis_slave/data:/data \
 	-v /home/wwwroot/cluster/redis_slave/redis.conf:/etc/redis/redis.conf \
