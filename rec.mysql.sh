655b25cc305bd0d8ea181f717aeeb10d31220c6b
wyq <cffycls@foxmail.com>
2021-10-29 17:24:32 +0800

    mysql版本测试

--- rec.mysql.sh
+++ rec.mysql.sh
@@ -23,3 +23,20 @@ docker run --name ms \
 
 echo -e "\n镜像启动后如果 mater & slave 没有正确关联, 在MySQL的主机登录`SHOW MASTER STATUS`查看参数、从机的命令行执行： "
 echo "change master to master_host='172.1.1.11',master_port=3306,master_user='repl',master_password='Ron_master_1',master_log_file='mysql-bin.000006',master_log_pos=155;"
+
+
+# 在宿主机下ping你配置的容器的，只要是172.50.0.0 段的都可以ping通
+docker network create mybridge --subnet=172.16.0.0/16
+docker run --name mysql56  \
+  --network=mybridge --ip=172.16.21.11 \
+  -e MYSQL_ROOT_PASSWORD=root \
+  -d mysql:5.6
+docker run --name mysql5624  \
+  --network=mybridge --ip=172.16.21.12 \
+  -e MYSQL_ROOT_PASSWORD=root \
+  -d mysql:5.6.24
+CREATE TABLE `set_test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` set('','0','1','2','3','4','5','6') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='set测试表';
